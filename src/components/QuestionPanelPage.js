3d23557b6b91599cae91b588870a9d6f2d08f7c8
Aarya Prasannan <aaryaprasannan@gmail.com>
2024-01-04 21:12:36 +0000

    questions popping up

--- src/components/QuestionPanelPage.js
+++ src/components/QuestionPanelPage.js
@@ -6,20 +6,37 @@ import Stomp from "stompjs";
 
 const QuestionPanelPage = (playerId, roomId) => {
   const [question, setQuestion] = useState("");
+
+  const sampleQues = {
+    id: "1",
+    question: "What is the capital of France?",
+    quizId: "mugesh_562",
+    options: {
+      OptionA: "Paris",
+      OptionB: "Berlin",
+      OptionC: "London",
+      OptionD: "Madrid",
+    },
+    correctAnswer: "OptionA",
+  };
+
   useEffect(() => {
     const socket = new SockJS("http://localhost:8080/game-socket");
     const stompClient = Stomp.over(socket);
 
     stompClient.connect({}, () => {
       stompClient.subscribe("/events/question", (response) => {
-        console.log(response);
-        setQuestion(JSON.parse(response.body).content);
+        console.log("Got a question that looks like this! ");
+        const newQuestion = JSON.parse(response.body);
+        console.log("Sending Format ");
+        console.log(newQuestion);
+        setQuestion(newQuestion);
       });
     });
 
-    return () => {
-      stompClient.disconnect();
-    };
+    // return () => {
+    //   stompClient.disconnect();
+    // };
   }, []);
 
   const handleSelectOption = (selectedOption) => {
@@ -31,16 +48,10 @@ const QuestionPanelPage = (playerId, roomId) => {
     <div>
       <Navbar currentPage="game-page" />
       <div className="container mx-auto mt-20 flex items-center justify-center">
-        {question ? (
-          <CurrentQuestion
-            question={question}
-            onSelectOption={handleSelectOption}
-          />
-        ) : (
-          <div>
-            <p>Waiting for incoming questions...</p>
-          </div>
-        )}
+        <CurrentQuestion
+          question={question}
+          onSelectOption={handleSelectOption}
+        />
       </div>
     </div>
   );
